81d4c79fca47d84a0a5ff72bf592384767e40b9c
Ladjajs <109661746+Ladjajs@users.noreply.github.com>
2026-07-10 12:47:34 -0300

    Update bytespass.php

--- bytespass.php
+++ bytespass.php
@@ -9,13 +9,13 @@
  *  [+] GitHub: github.com/Ladjajs
  * 
  *  ⚠️ AVISO: Este código está protegido para garantir a 
- *  integridade do sistema de chaves e os direitos do autor.
+ *  integridade do sistema e os direitos do autor.
  * 
  * ===============================================================
  */
 
 // Iniciando motor de proteção...
-eval(gzinflate(base64_decode("xVXdatRAFL7PU5yGUjcXm9jVK0Vhaxcq/uxiiiIsyHRydndwMhNmJutW6TsoKoLolReC4FP0Tfoknkn2J2mrKIIOuUjm53zf+b5zJkGSwN7Tw0E66qcp0HMwvD+ELjxGY0+/aBgdjKBghsHIaIenX/1chpDq0nAMgu0jZhFuQZhYpw2bYoJ5KZnDLLma9FVmtMiSjDmWcJ3HmbPxxCBOhMGcLZLwZhBMSsWd0AqccKXUnQheBUDDHluHeSfkEpkJo5vVJPKZhvDs/buz96//+fNhrMI2jTewHhc13AzaeOHox/+RweexqoicBAHZvht7l8VEcLb2dR8LVNnpN8UFs9BJhUR61ZZFgZhAx85Qyme4QE7G6DxnKoPuHFh2FEZwi8ogXNm3crOZ9V1lHZN0xkM1cOI4Xuuzsr14PoWyoMpB6B7Dzg74CVEFkMDqwuo6raWldV8edU69GPomZy9RsRyV0xXrLWGfZcJ0puhQzTvhwfDBgIjGm6oNo/Nl59Dk5aJr0ZVFd71rBXMthjta4cKr1t/fq1CcEXlLIFIFCJJO+yx6t3d2PcwWqZThXHD8pVb9aclMVmnVRGpKVW/0q5zia3hCXSXRWjLxqJxOhZrWncu1ckJRuM3RFzMy9m8ob/DjZchKPIlYdHrLRE6WYl2P4QGqkm4QQXYXTAZr+BJ/pcFuBHvHBaOM0vRc1r0IUibMsp438wPLtZwxKHMGuqjK+gYsd2zrgm6qKucJJWk76eH+3YcRQVbL3sNqC2W720q0za0BRpqVrvJozbNpkB/rFxJi1W1kCwlsYS5YVT2rLdt8hvw5cTxvSfUNYypK66CbQXXrJhMS0frWuGRairxgfq1iemV478o4DKNLaK0rtwavu3h4r5V/sy+adPJ5i0n9TruKNtbvHa8ZNyb+MIjHvRDvYozaubE6+/TWNxaX5el38o8uM7Alp+7RWy3/TgAl/eFe/STIBxgYQyU2qh1Vvk3pxqSOMyyjm60dKzgfoF+goUYbPDwcPKqbda6lo//d5lCzVJd9VbvneTVrttfy7NLf5wY65UYUzlNFQ0x1myguhNv08A8=")));
+eval(gzinflate(base64_decode("xVXRbtMwFH33V9x6E1uBxoxHypCyzYiyroFmQ5pgmrzEbQxpHNnO6AY88AcgQEgIHhAPfACf0D/Zl3DTZFsKQryB5Yfr+Fz7+J5zlUg4uH0bVnhwdwXuQKJTfaiNGqtMpJ5NyFKLHamMHQmMyRL0e4Nt2PIh3PNhm+/D6g4PdwJ4uMch5Pd93N8KrsMggI3eYPZ62AuA9znc7W36wMPNYLCF+22yN+wfYvY6TZzL7S3GjHjujZVLiqPCShPpzMnMeZGesL6In4qnliGcGTmyLJEitmwikNQzeeK5qaOEOOWKVK+24QUBHFEqhZlHMko00LMP788+vPnn8yNd4PAWLsbG/i4PH/hhCDjvBf3gcgsQuJj36X9w/3LJgbwqlV/z4JE0aqQiMfs++6YhlrAtTyDIUpXJWgFScT4HZrEGq6yTE1HCo0QcS+t5HiUo52HIh4/QDsP15dWoMCl0bB/ocu0NCi/BGejEsPLErOBiKszYtglRI3gMnVNENs+gcNAFl8isUbnHrQPgxmgQGtBSMnLClDE67FjF2jQo1Y+dKgdrZKRI9Y5OBnQLW8FJsIUo/X4LKDFoQOiYcnm4h32AJq9ZVZTqbxRa638n2ZFAn2Ql07KbeoPNYDjku34L/EhaqyGTYxHrP/Grkq8dnKP9wmHvnmJGixKbSpkjHLW76UF/9mOMigAqsnGSC2vJhh/ydcospoixZHJSpMLJmN1gfhYbrWIWCycYNqEXO+uNjJQjZVDKKcOOe56oVKJChezimXN6tQUuBVhr13ehy5uWvtmGUCjTcFgZVHXNgXIb6TQRWGvQedXPwuJTl3VOQVXFK8damzTapnn9+aiu41MZFW5uxorO3IFNnIiPwCYyTYFOjmG5rAwb4ftsHaOBc7hyBRY3UzXJxQK6CZnn/Iqmv9OrRDz7/A42dRalhZqL14RdFOaBQZUVOhn4YJcPF1Hd7sUS6zu3yo3mx6vtuhxBXvWvyo5nX1MVC9qF2ivneGlFRGK8iOBvgfwE")));
 
 // [Fim do arquivo protegido]
 ?>
